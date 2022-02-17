bbb318ba4d78c7b3b1f89df2305356c6bb47f8e8
testhi12345 <99924440+testhi12345@users.noreply.github.com>
2022-02-17 15:24:26 -0700

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "brightlylearning@gmail.com";
-$subject = "Go Away!";
-$txt = "Now I am a hacker. I can email from the pipecleaner world!";
-$headers = "From: iamthebestest@pipecleanerworld.piw" . "\r\n" .
-"CC: htest577@gmail.com";
-
-mail($to,$subject,$txt,$headers);
-?>
